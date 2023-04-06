886af6b7e87adec684d5f2072246f4b406e100ca
akshat.aggarwal <akshat.aggarwal@iongroup.com>
2023-04-06 23:32:31 +0530

    Dec timer time

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,52 +1,52 @@
-(function($) {
+(function ($) {
     'use strict';
 
     // Mean Menu JS
-    jQuery('.mean-menu').meanmenu({ 
+    jQuery('.mean-menu').meanmenu({
         meanScreenWidth: "991"
     });
 
     // Navbar Area
-    $(window).on('scroll', function() {
-        if ($(this).scrollTop() >150){  
+    $(window).on('scroll', function () {
+        if ($(this).scrollTop() > 150) {
             $('.navbar-area').addClass("sticky-nav");
         }
-        else{
+        else {
             $('.navbar-area').removeClass("sticky-nav");
         }
     });
 
     // FAQ Accordion JS
-	$('.accordion').find('.accordion-title').on('click', function(){
-		// Adds Active Class
-		$(this).toggleClass('active');
-		// Expand or Collapse This Panel
-		$(this).next().slideToggle('fast');
-		// Hide The Other Panels
-		$('.accordion-content').not($(this).next()).slideUp('fast');
-		// Removes Active Class From Other Titles
-		$('.accordion-title').not($(this)).removeClass('active');		
-	});
+    $('.accordion').find('.accordion-title').on('click', function () {
+        // Adds Active Class
+        $(this).toggleClass('active');
+        // Expand or Collapse This Panel
+        $(this).next().slideToggle('fast');
+        // Hide The Other Panels
+        $('.accordion-content').not($(this).next()).slideUp('fast');
+        // Removes Active Class From Other Titles
+        $('.accordion-title').not($(this)).removeClass('active');
+    });
 
     // Brand Slider 
-     $('.brand-slider').owlCarousel({
+    $('.brand-slider').owlCarousel({
         loop: true,
         margin: 30,
         nav: false,
         dots: false,
         autoplay: true,
         autoplayHoverPause: true,
-        responsive:{
-            0:{
+        responsive: {
+            0: {
                 items: 2
             },
-            568:{
+            568: {
                 items: 3
             },
-            768:{
+            768: {
                 items: 5
             },
-            1000:{
+            1000: {
                 items: 5
             }
         }
@@ -64,14 +64,14 @@
             "<i class='bx bx-left-arrow-alt'></i>",
             "<i class='bx bx-right-arrow-alt'></i>"
         ],
-        responsive:{
-            0:{
+        responsive: {
+            0: {
                 items: 1
             },
-            768:{
+            768: {
                 items: 2
             },
-            1000:{
+            1000: {
                 items: 3
             }
         }
@@ -104,27 +104,27 @@
             "<i class='bx bx-left-arrow-alt'></i>",
             "<i class='bx bx-right-arrow-alt'></i>"
         ],
-        responsive:{
-            0:{
+        responsive: {
+            0: {
                 items: 1
             },
-            768:{
+            768: {
                 items: 2
             },
-            1000:{
+            1000: {
                 items: 3
             }
         }
     })
 
     // Tabs
-    $('#tabs-item li a').on('click', function(e) {
+    $('#tabs-item li a').on('click', function (e) {
         $('#tabs-item li, #prices-content .active').removeClass('active').removeClass('fadeInUp');
         $(this).parent().addClass('active');
         var activeTab = $(this).attr('href');
-        $( activeTab).addClass('active fadeInUp');
+        $(activeTab).addClass('active fadeInUp');
         e.preventDefault();
-    });	
+    });
 
     // Popup Video 
     $('.play-btn').magnificPopup({
@@ -149,26 +149,26 @@
             "<i class='bx bx-left-arrow-alt'></i>",
             "<i class='bx bx-right-arrow-alt'></i>"
         ],
-        responsive:{
-            0:{
+        responsive: {
+            0: {
                 items: 1
             },
-            768:{
+            768: {
                 items: 2
             },
-            1000:{
+            1000: {
                 items: 3
             }
         }
     })
 
     // Search Botton
-    $('.close-btn').on('click',function() {
+    $('.close-btn').on('click', function () {
         $('.search-overlay').fadeOut();
         $('.search-btn').show();
         $('.close-btn').removeClass('active');
     });
-    $('.search-btn').on('click',function() {
+    $('.search-btn').on('click', function () {
         $(this).hide();
         $('.search-overlay').fadeIn();
         $('.close-btn').addClass('active');
@@ -185,7 +185,7 @@
             event.preventDefault();
         }
     });
-    function callbackFunction (resp) {
+    function callbackFunction(resp) {
         if (resp.result === "success") {
             formSuccessSub();
         }
@@ -193,28 +193,28 @@
             formErrorSub();
         }
     }
-    function formSuccessSub(){
+    function formSuccessSub() {
         $(".newsletter-form")[0].reset();
         submitMSGSub(true, "Thank you for subscribing!");
-        setTimeout(function() {
+        setTimeout(function () {
             $("#validator-newsletter").addClass('hide');
         }, 4000)
     }
-    function formErrorSub(){
+    function formErrorSub() {
         $(".newsletter-form").addClass("animated shake");
-        setTimeout(function() {
+        setTimeout(function () {
             $(".newsletter-form").removeClass("animated shake");
         }, 1000)
     }
-    function submitMSGSub(valid, msg){
-        if(valid){
+    function submitMSGSub(valid, msg) {
+        if (valid) {
             var msgClasses = "validation-success";
         } else {
             var msgClasses = "validation-danger";
         }
         $("#validator-newsletter").removeClass().addClass(msgClasses).text(msg);
     }
-        
+
     // AJAX MailChimp
     $(".newsletter-form").ajaxChimp({
         url: "https://envyTheme.us20.list-manage.com/subscribe/post?u=60e1ffe2e8a68ce1204cd39a5&amp;id=42d6d188d9", // Your url MailChimp
@@ -223,14 +223,14 @@
 
     // Back To Top Js
     $('body').append('<div id="toTop" class="top-btn"><i class="bx bx-chevrons-up"></i></div>');
-    $(window).on('scroll',function () {
+    $(window).on('scroll', function () {
         if ($(this).scrollTop() != 0) {
             $('#toTop').fadeIn();
         } else {
             $('#toTop').fadeOut();
         }
-    }); 
-    $('#toTop').on('click',function(){
+    });
+    $('#toTop').on('click', function () {
         $("html, body").animate({ scrollTop: 0 }, 600);
         return false;
     });
@@ -239,8 +239,8 @@
     new WOW().init();
 
     // Preloader JS
-    jQuery(window).on('load',function(){
-        jQuery(".preloader").fadeOut(500);
+    jQuery(window).on('load', function () {
+        jQuery(".preloader").fadeOut(100);
     });
 
 })(jQuery);
